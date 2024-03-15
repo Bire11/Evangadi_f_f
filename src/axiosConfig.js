f7384ff74725c9b598afdef9c34e83965e268d12
unknown <birhanuwoeldmariam@gmail.com>
2024-03-15 12:17:10 +0300

    one

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 const axiosBase = axios.create({
   
-  baseURL: "http://localhost:2001/api",
+  // baseURL: "http://localhost:2001/api",
+  baseURL: "https://birhanu-final.cyclic.app/api",
 });
 export default axiosBase;
 
